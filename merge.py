b7cfb0996ab70738b8088fbc8668c5e66322ddec
daniel <shpedagod@gmail.com>
2023-06-09 13:25:17 +0300

    egh cgbcjre

---
+++ merge.py
@@ -0,0 +1,33 @@
+
+
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+list_1 = [1, 5, 9, 2, 19, 6, 7, 9, 11]
+merge_sort(list_1)
+print(list_1)
+
